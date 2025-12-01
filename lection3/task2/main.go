18c94255075fca6319da9c2470d1c37633681109
Maxim <makcmoscow@gmail.com>
2025-12-01 14:44:54 +0300

    task2

---
+++ lection3/task2/main.go
@@ -0,0 +1,16 @@
+// Программа запрашивает имя пользователя и возраст и выводит их. Возраст за вычетом одного года.
+package main
+
+import "fmt"
+
+
+func main(){
+	var value string
+
+	fmt.Print("Введите значение, три символа: ")
+	fmt.Scan(&value)
+	fmt.Println(string(rune(value[0])))
+	fmt.Println(string(rune(value[1])))
+	fmt.Println(string(rune(value[2])))
+
+}
