9556de017a73b19e77d429e49452ccd2a577c9d7
Maxim <makcmoscow@gmail.com>
2025-12-11 12:10:01 +0300

    Store in progress

--- golang_lessons_2/09122025/Lection4_dbStore/Bankstore/db/sqlc/account_test.go
+++ golang_lessons_2/09122025/Lection4_dbStore/Bankstore/db/sqlc/account_test.go
@@ -3,67 +3,110 @@
 */
 package db
 import (
-    // "fmt"
+    "Bankstore/utils"
     "context"
-    "os"
     "log"
+    "os"
     "testing"
+    "time"
     "github.com/jackc/pgx/v5"
-	"github.com/stretchr/testify/require"
-	u "Bankstore/utils"
+    "github.com/stretchr/testify/require"
 )
 const (
     dbSource = "postgresql://app_user:pswd@localhost:5432/bankdb?sslmode=disable"
 )
+// Глобальный контекст для работы с БД и тестами
 var ctx = context.Background()
+// Декларация переменной
 var testQueries *Queries
 func TestMain(m *testing.M) {
+    // Соединение с БД
     conn, err := pgx.Connect(ctx, dbSource)
     if err != nil {
         log.Fatal("can not connect to db", err)
     }
+    // Закрываем соединение
     defer conn.Close(ctx)
+    // Вызываем конструктор для создания экземпляра типа данных Queries
     testQueries = New(conn)
+    // Запускаем subtest(тесты) и итоговый код выполнения передаем в Exit()
     os.Exit(m.Run())
 }
 func TestCreateAccount(t *testing.T) {
-	createRandomAccount(t)
+    createRandomAccount(t)
 }
-
 func createRandomAccount(t *testing.T) Account {
-	    arg := CreateAccountParams{
-		Owner: u.RandomAccount().Owner,
-		Balance: u.RandomAccount().Balance,
-		Currency: Currency(u.RandomAccount().Currency),
-	}
-	account, err := testQueries.CreateAccount(ctx, arg)
-	//Две проверки на результат работы CreateAccount
-	require.NoError(t, err)
-	require.NotEmpty(t, account)
-
-	require.Equal(t, arg.Owner, account.Owner)
-	require.Equal(t, arg.Balance, account.Balance)
-	require.Equal(t, arg.Balance, account.Balance)
-
-	require.NotZero(t, account.ID)
-	require.NotZero(t, account.CreatedAt)
-
-	return account
+    ra := utils.RandomAccount()
+    arg := CreateAccountParams {
+        Owner: ra.Owner,
+        Balance: utils.RandomInt(0, 1000),
+        // Balance: ra.Balance,
+        Currency: Currency(ra.Currency),
+    }
+    account, err := testQueries.CreateAccount(ctx, arg)
+    
+    // Две проверки на результат работы CreateAccount
+    require.NoError(t, err)
+    require.NotEmpty(t, account)
+    // Дополнительные проверки
+    require.Equal(t, arg.Owner, account.Owner)
+    require.Equal(t, arg.Balance, account.Balance)
+    require.Equal(t, arg.Currency, account.Currency)
+    require.NotZero(t, account.ID)
+    require.NotZero(t, account.CreatedAt)
+    return account
 }
-
-func TestUpdateAccount(t *testing.T) Account {
-		arg := CreateAccountParams{
-		Owner: u.RandomAccount().Owner,
-		Balance: u.RandomAccount().Balance,
-		Currency: Currency(u.RandomAccount().Currency),
-	}
+func TestGetAccount(t *testing.T) {
+    account1 := createRandomAccount(t)
+    account2, err := testQueries.GetAccount(ctx, account1.ID)
+    require.NoError(t, err)
+    require.NotEmpty(t, account2)
+    // test all fields
+    require.Equal(t, account1.ID, account2.ID)
+    require.Equal(t, account1.Owner, account2.Owner)
+    require.Equal(t, account1.Balance, account2.Balance)
+    require.Equal(t, account1.Currency, account2.Currency)
+    require.WithinDuration(t, account1.CreatedAt.Time, account2.CreatedAt.Time, time.Second)
 }
-
-/*
-Реализовать тесты для остальных cRUD’ов типа Account, т.е. для:
-
-GetAccount (подсказка для поля CreatedAt: account.CreatedAt.Time)
-ListAccounts
-UpdateAccount
-DeleteAccount
-*/
+func TestUpdateAccount(t *testing.T) {
+    account1 := createRandomAccount(t)
+    arg := UpdateAccountParams {
+        ID: account1.ID,
+        Balance: utils.RandomInt(0, 1000),
+    }
+    account2, err := testQueries.UpdateAccount(ctx, arg)
+    require.NoError(t, err)
+    require.NotEmpty(t, account2)
+    // test all fields
+    require.Equal(t, account1.ID, account2.ID)
+    require.Equal(t, account1.Owner, account2.Owner)
+    require.Equal(t, arg.Balance, account2.Balance)
+    require.Equal(t, account1.Currency, account2.Currency)
+    require.WithinDuration(t, account1.CreatedAt.Time, account2.CreatedAt.Time, time.Second)
+}
+func TestDeleteAccount(t *testing.T) {
+    account1 := createRandomAccount(t)
+    // Delete created account
+    err := testQueries.DeleteAccount(ctx, account1.ID)
+    require.NoError(t, err)
+    account2, err := testQueries.GetAccount(ctx, account1.ID)
+    require.Error(t, err)
+    require.Empty(t, account2)
+    // проверка на тип ошибки(нуль строк было обработано)
+    require.EqualError(t, err, pgx.ErrNoRows.Error())    
+}
+func TestListAccounts(t *testing.T) {
+    for range 10 {
+        createRandomAccount(t)
+    }
+    arg := ListAccountsParams{
+        Limit: 8,
+        Offset: 0,
+    }
+    accounts, err := testQueries.ListAccounts(ctx, arg)
+    require.NoError(t, err)
+    require.Len(t, accounts, 8)
+    for _, acc := range accounts {
+        require.NotZero(t, acc)
+    }
+}
---
+++ golang_lessons_2/09122025/Lection4_dbStore/Bankstore/db/sqlc/store.go
@@ -0,0 +1,82 @@
+package db
+import (
+    "context"
+    "fmt"
+    "github.com/jackc/pgx/v5"
+    "github.com/jackc/pgx/v5/pgxpool"
+)
+type Store struct {
+    db *pgxpool.Pool
+    *Queries
+}
+func NewStore(db *pgxpool.Pool) *Store {
+    return &Store {
+        db: db,
+        Queries: New(db),
+    }
+}
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+    tx, err := store.db.BeginTx(ctx, pgx.TxOptions{})
+    if err != nil {
+        return err
+    }
+    q := New(tx) // создаем экземпляр Queries на основе pgx.Tx
+    err = fn(q) // обработка созданного экземпляра
+    if err != nil {
+        if rbErr := tx.Rollback(ctx); rbErr != nil {
+            return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+        }
+        return err
+    }
+    return tx.Commit(ctx)
+}
+
+// Начинаем реализацию перевода через транзакцию
+
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID int64 `json:"to_account_id"`
+	Amount int64 `json:"amount"`
+}
+
+type TransferTxResult struct {
+	Transfer Transfer `json:"transfer"`
+		FromAccountID int64 `json:"from_account_id"`
+	ToAccountID int64 `json:"to_account_id"`
+	FromEntry Entry `json:"from_entry"`
+	ToEntry Entry `json:"to_entry"`
+}
+
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+	err := store.execTx(ctx, func (q *Queries) error {
+		// Step 1. Create Transfer record
+		var err error
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams(arg))
+		if err != nil {
+			return err
+		}
+
+		// Step 2. Create two entry records
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount: -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount: arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		// Step 3. Update account's balance
+		// TODO
+		return nil
+	})
+	return result, err
+}
