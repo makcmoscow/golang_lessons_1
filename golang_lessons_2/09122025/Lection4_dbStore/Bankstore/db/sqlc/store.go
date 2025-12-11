b083142d5f48412af05a4994b8f08ec8ca7d770e
Maxim <makcmoscow@gmail.com>
2025-12-11 16:55:03 +0300

    add test of context info

--- golang_lessons_2/09122025/Lection4_dbStore/Bankstore/db/sqlc/store.go
+++ golang_lessons_2/09122025/Lection4_dbStore/Bankstore/db/sqlc/store.go
@@ -10,7 +10,7 @@ type Store struct {
     *Queries
 }
 func NewStore(db *pgxpool.Pool) *Store {
-    return &Store {
+    return &Store{
         db: db,
         Queries: New(db),
     }
@@ -30,53 +30,85 @@ func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
     }
     return tx.Commit(ctx)
 }
-
 // Начинаем реализацию перевода через транзакцию
-
 type TransferTxParams struct {
-	FromAccountID int64 `json:"from_account_id"`
-	ToAccountID int64 `json:"to_account_id"`
-	Amount int64 `json:"amount"`
+    FromAccountID int64 `json:"from_account_id"`
+    ToAccountID int64 `json:"to_account_id"`
+    Amount int64 `json:"amount"`
+}
+type TransferTxResult struct {
+    Transfer Transfer `json:"transfer"`
+    FromAccount Account `json:"from_account"`
+    ToAccount Account `json:"to_account"`
+    FromEntry Entry `json:"from_entry"`
+    ToEntry Entry `json:"to_entry"`
 }
 
-type TransferTxResult struct {
-	Transfer Transfer `json:"transfer"`
-		FromAccountID int64 `json:"from_account_id"`
-	ToAccountID int64 `json:"to_account_id"`
-	FromEntry Entry `json:"from_entry"`
-	ToEntry Entry `json:"to_entry"`
-}
+var txKey = struct{}{}
 
 func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
-	var result TransferTxResult
-	err := store.execTx(ctx, func (q *Queries) error {
-		// Step 1. Create Transfer record
-		var err error
-		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams(arg))
-		if err != nil {
-			return err
-		}
+    var result TransferTxResult
+    err := store.execTx(ctx, func(q *Queries) error {
+        // Step 1. Create Transfer record
+        var err error
 
-		// Step 2. Create two entry records
-		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
-			AccountID: arg.FromAccountID,
-			Amount: -arg.Amount,
-		})
-		if err != nil {
-			return err
-		}
-
-		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
-			AccountID: arg.ToAccountID,
-			Amount: arg.Amount,
-		})
-		if err != nil {
-			return err
-		}
-
-		// Step 3. Update account's balance
-		// TODO
-		return nil
-	})
-	return result, err
+		txName := ctx.Value(txKey)
+		fmt.Println(txName, "create transfer")
+        result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams(arg))
+        if err != nil {
+            return err
+        }
+        // Step 2. Create two Entry records
+		fmt.Println(txName, "create entry 1")
+        result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+            AccountID: arg.FromAccountID,
+            Amount: -arg.Amount,
+        })
+        if err != nil {
+            return err
+        }
+        result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+            AccountID: arg.ToAccountID,
+            Amount: arg.Amount,
+        })
+        if err != nil {
+            return err
+        }
+        // Step 3. Update account's balance
+        // =========
+        // Wrong way -> right way
+        // =========
+        // ================================
+        // move money out of account1
+		fmt.Println(txName, "get account 1")
+        account1, err := q.GetAccountForUpdate(ctx, arg.FromAccountID)
+        if err != nil {
+            return err
+        }
+		fmt.Println(txName, "update account 1")
+        result.FromAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
+            ID: arg.FromAccountID,
+            Balance: account1.Balance - arg.Amount,
+        })
+        if err != nil {
+            return err
+        }
+        // move money to account2
+		fmt.Println(txName, "get account 2")
+        account2, err := q.GetAccountForUpdate(ctx, arg.ToAccountID)
+        if err != nil {
+            return err
+        }
+		fmt.Println(txName, "update account 1")
+        result.ToAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
+            ID: arg.ToAccountID,
+            Balance: account2.Balance + arg.Amount,
+        })
+        if err != nil {
+            return err
+        }        
+        // ================================
+        return nil
+    })
+    return result, err
 }
--- golang_lessons_2/09122025/Lection4_dbStore/Bankstore/db/sqlc/store_test.go
+++ golang_lessons_2/09122025/Lection4_dbStore/Bankstore/db/sqlc/store_test.go
@@ -1,28 +1,96 @@
 package db
-
 import (
-	"context"
-	"fmt"
-	"testing"
+    "context"
+    "fmt"
+    "testing"
+    "github.com/stretchr/testify/require"
 )
-
 func TestTransferTx(t *testing.T) {
-	store := NewStore(testDB)
-
-	account1 := createRandomAccount(t)
-	account2 := createRandomAccount(t)
-	fmt.Println("Before:", account1.Balance, account2.Balance)
-	
-	amount:= int64(10)
-	// fmt.Printf("Context.Background(): %#v", context.Background())
-
-	result, err := store.TransferTx(context.Background(), TransferTxParams{
-		FromAccountID: account1.ID,
-		ToAccountID: account2.ID,
-		Amount: int64(amount),
-	})
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Printf("Result: %v\n", result)
+    store := NewStore(testDB) // testDB == nil -> store == nil
+    account1 := createRandomAccount(t)
+    account2 := createRandomAccount(t)
+    fmt.Println("Before:", account1.Balance, account2.Balance)
+    n := 5
+    amount := int64(10)
+    errs := make(chan error)
+    results := make(chan TransferTxResult)
+    // run n concurrent transfer transactions
+    for i := range(n) {
+		txName := fmt.Sprintf("tx %d", i+1)
+        go func() {
+			ctx := context.WithValue(context.Background(), txKey, txName)
+            result, err := store.TransferTx(ctx, TransferTxParams{
+                FromAccountID: account1.ID,
+                ToAccountID: account2.ID,
+                Amount: amount,
+            })
+            errs <- err
+            results <- result
+            }()
+        }
+    // Check results
+    existed := make(map[int]bool)
+    for range n {
+        // check errors
+        err := <- errs
+        require.NoError(t, err)
+        // check transfers
+        result := <- results
+        transfer := result.Transfer
+        require.NotEmpty(t, transfer)
+        require.Equal(t, account1.ID, transfer.FromAccountID)
+        require.Equal(t, account2.ID, transfer.ToAccountID)
+        require.Equal(t, amount, transfer.Amount)
+        require.NotZero(t, transfer.ID)
+        require.NotZero(t, transfer.CreatedAt)
+        // Additional check
+        _, err = store.GetTransfer(context.Background(), transfer.ID)
+        require.NoError(t, err)
+        // check entries
+        fromEntry := result.FromEntry
+        require.NotEmpty(t, fromEntry)
+        require.Equal(t, account1.ID, fromEntry.AccountID)
+        require.Equal(t, -amount, fromEntry.Amount)
+        require.NotZero(t, fromEntry.ID)
+        require.NotZero(t, fromEntry.CreatedAt)
+        _, err = store.GetEntry(context.Background(), fromEntry.ID)
+        require.NoError(t, err)
+        toEntry := result.ToEntry
+        require.NotEmpty(t, toEntry)
+        require.Equal(t, account2.ID, toEntry.AccountID)
+        require.Equal(t, amount, toEntry.Amount)
+        require.NotZero(t, toEntry.ID)
+        require.NotZero(t, toEntry.CreatedAt)
+        _, err = store.GetEntry(context.Background(), toEntry.ID)
+        require.NoError(t, err)
+        // check accounts
+        fromAccount := result.FromAccount
+        require.NotEmpty(t, fromAccount)
+        require.Equal(t, account1.ID, fromAccount.ID)
+        toAccount := result.ToAccount
+        require.NotEmpty(t, toAccount)
+        require.Equal(t, account2.ID, toAccount.ID)
+        // check account's balance
+        fmt.Println(">> Tx", account1.Balance, account2.Balance)
+        diff1 := account1.Balance - fromAccount.Balance
+        diff2 := toAccount.Balance - account2.Balance
+        require.Equal(t, diff1, diff2)
+        require.True(t, diff1 > 0)
+        require.True(t, diff1 % amount == 0)
+        k := int(diff1 / amount)
+        require.True(t, k >=1 && k <= n)
+        require.NotContains(t, existed, k)
+        existed[k] = true
+    }
+    // После отработки n горутин,проверяем результаты работы(в первую очередь, балансы)
+    // final check
+    updateAccount1, err := store.GetAccount(context.Background(), account1.ID)
+    require.NoError(t, err)
+    updateAccount2, err := store.GetAccount(context.Background(), account2.ID)
+    require.NoError(t, err)
+    fmt.Println("After:", updateAccount1.Balance, updateAccount2.Balance)
+    // Добавляем все списания
+    require.Equal(t, account1.Balance, updateAccount1.Balance + amount * int64(n))
+    // Вычитаем все начисления
+    require.Equal(t, account2.Balance, updateAccount2.Balance - amount * int64(n))
 }
